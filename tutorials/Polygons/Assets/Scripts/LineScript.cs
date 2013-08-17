9e5ba2bae21fbd661c53a69f334404714642561d
WooKyoung Noh <wookay.noh@gmail.com>
2013-08-18 00:36:41 +0900

    Polygons

---
+++ tutorials/Polygons/Assets/Scripts/LineScript.cs
@@ -0,0 +1,23 @@
+﻿// LineScript.cs
+// wookay.noh@gmail.com
+
+using UnityEngine;
+using System.Collections;
+
+public class LineScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.SetVertexCount(4);
+		lineRenderer.SetPosition(0, GameObject.Find("PointA").transform.position);
+		lineRenderer.SetPosition(1, GameObject.Find("PointB").transform.position);
+		lineRenderer.SetPosition(2, GameObject.Find("PointC").transform.position);
+		lineRenderer.SetPosition(3, GameObject.Find("PointA").transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ tutorials/Polygons/Assets/Scripts/PointScript.cs
@@ -0,0 +1,42 @@
+﻿// PointScript.cs
+// wookay.noh@gmail.com
+
+using UnityEngine;
+using System.Collections;
+
+public class PointScript : MonoBehaviour {
+	
+	public GameObject line;
+	
+	// Use this for initialization
+	void Start () {
+		line = GameObject.Find("Line");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	IEnumerator OnMouseDown() {
+        Vector3 scrSpace = Camera.main.WorldToScreenPoint (transform.position);
+        Vector3 offset = transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, scrSpace.z));
+         
+        while (Input.GetMouseButton(0)) {
+            Vector3 curScreenSpace = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, scrSpace.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+            transform.position = curPosition;
+			
+			LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+			if ("PointA" == gameObject.name) {
+        		lineRenderer.SetPosition(0, curPosition);
+				lineRenderer.SetPosition(3, curPosition);
+			} else if ("PointB" == gameObject.name) {
+				lineRenderer.SetPosition(1, curPosition);
+			} else if ("PointC" == gameObject.name) {
+				lineRenderer.SetPosition(2, curPosition);
+			}
+
+            yield return null;
+        }
+    }
+}
